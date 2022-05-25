ce1cb84330d4c4229c8454e59fbb85699266a634
Mohammed Abdulraziq <moh.ab@aerbag.app>
2022-05-25 14:03:00 +0200

    FCFS Working  Perfectly

--- FCFS.c
+++ FCFS.c
@@ -8,8 +8,8 @@ struct process_struct
 	int at; // Arrival Time
 	int pt; // CPU Burst time
 	int bt;
-	int ct, wt, tat, rt, start_time; // Completion, waiting, turnaround, response time
-} ps[100], rq[30], bq[100], current_proccess;
+	int tr;// Completion, waiting, turnaround, response time
+} ps[100], rq[30], bq[100], current_proccess,out[100];
 
 // FUNCTIONS
 int findmax(int a, int b)
@@ -41,6 +41,7 @@ int main()
 {
 
 	int n;
+	int i = 0;
 	// Run Queue
 	int rqe = 0;
 	int rqf = 0;
@@ -51,6 +52,9 @@ int main()
 	int rft = 0;
 	int is_running = 0;
 	int conflict = 0;
+	// usage
+	int usageCounter = 0;
+	int endFlag = 1;
 
 	printf("Enter total number of processes: ");
 	scanf("%d", &n);
@@ -59,6 +63,7 @@ int main()
 	for (int i = 0; i < 30; i++)
 	{
 		rq[i].pid = 999;
+		rq[i].tr = 0;
 	}
 	// STANDARD SCAN
 	for (int i = 0; i < n; i++)
@@ -73,40 +78,41 @@ int main()
 		scanf("%d", &ps[i].at);
 	}
 	
-	
+	int howManyAreIn = 0;
 	// LOGIC
-	for (int i = 0; i < 30; i++)
+	while (endFlag != 0 || howManyAreIn != n)
 	{
 
 		int block = 0;
+		endFlag = 0;
 
 		printf("\n%d ", i);
-
 
 		// CHECK BQ
 		if (bqf != 0)
 		{
 
-			// printf("bqe %d ", bqf);
-			// printf("bqf %d ", bqf);
 			for (int j = bqe; j < bqf; j++)
 			{
 
 				if (bq[j].bt != 0)
 				{
+					bq[j].tr++; 
 					bq[j].bt = bq[j].bt - 1;
 					printf("%d: BLOCKED ", bq[j].pid);
-					// block=1;
+					endFlag = 1;
 				}
 
 				else
 				{
 					rq[rqf] = bq[j];
 					bqe++;
+					// f is bigger
 					rqf++;
 				}
 
 			}
+
 		}
 
 		// CHECK RFT
@@ -115,16 +121,17 @@ int main()
 
 			if (current_proccess.bt == 0)
 			{
-				// printf("%D ENDED\n", current_proccess.pid);
-				// rqe++;
+				out[current_proccess.pid] = current_proccess;
 			}
 			else if(!block)
 			{
+				current_proccess.tr++;
 				bq[bqf] = current_proccess;
 				bq[bqf].bt = bq[bqf].bt - 1;
 				bqf++;
 
 				printf("%d: BLOCKED ", current_proccess.pid);
+				endFlag = 1;
 			}
 			is_running = 0;
 
@@ -133,6 +140,9 @@ int main()
 		// CHECK AT
 		for (int k = 0; k < n; k++)
 		{
+			if (ps[k].at == i){
+				howManyAreIn++;
+			}
 			if (ps[k].at == i && ps[k].pt != 0)
 			{
 				rq[rqf] = ps[k];
@@ -146,35 +156,42 @@ int main()
 
 			if (is_running)
 			{
+				current_proccess.tr++;
 				printf("%d: RUNNING ", current_proccess.pid);
+				usageCounter++;
+				endFlag = 1;
 				for (int k = 0; k < rqf - rqe; k++)
 				{
+					rq[k + rqe].tr++;
 					printf("%d: READY ",rq[k + rqe].pid);
+					endFlag = 1;
+
 				}
 			}
 
 			if (!is_running && rqf != rqe)
 			{
-
-				// printf("rqf %d\n",rqf);
-				// printf("rqe %d\n",rqe);
 				
 				struct process_struct temp[rqf - rqe];
+
 				for (int k = 0; k < rqf - rqe; k++)
 				{
 					temp[k] = rq[rqe + k];
-					// if (k != 0) printf("%d: READY ",rq[k + rqe].pid);
 				}
 
 				qsort((void *)temp, rqf - rqe, sizeof(struct process_struct), comparatorPID);
 
 				for (int k = 0; k < rqf - rqe; k++)
 				{
+					temp[k].tr++;
 					rq[rqe + k] = temp[k];
 					if (k != 0) printf("%d: READY ",rq[k + rqe].pid);
+					endFlag = 1;
 				}
 
 				printf("%d: RUNNING ", temp[0].pid);
+				usageCounter++;
+				endFlag = 1;
 				rft = i + temp[0].pt;
 				is_running = 1;
 				current_proccess = temp[0];
@@ -183,6 +200,14 @@ int main()
 			
 			
 		}
+		i++;
+	}
+
+	printf("\nFinishing Time : %d\n",i - 2);
+	float utlization = (float)usageCounter/(float)(i-1);
+	printf("CPU Utilization : %f\n",utlization);
+	for (int p = 0; p < n; p++){
+		printf("Turnaround Time of Proccess : %d: %d\n",out[p].pid,out[p].tr);
 	}
 
 	return 0;
